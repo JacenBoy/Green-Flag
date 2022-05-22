9019e3fc4303718b80ca06b0fa70687d793c72c8
JacenBoy <jacenboy1@gmail.com>
2022-05-22 11:55:32 -0400

    Add event notes box and start work on adding notes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const screen = blessed.screen({
 });
 
 const scoringBox = blessed.box({
-  width: "100%",
+  width: "75%",
   height: "85%",
   top: "15%",
   left: "0%",
@@ -43,18 +43,30 @@ const lapsBox = blessed.box({
 });
 screen.append(lapsBox);
 
-const eventBox = blessed.box({
+const eventBox = blessed.log({
   width: "75%",
   height: "15%",
   top: "0%",
   left: "0%",
   align: "center",
   valign: "middle",
+  scrollOnInput: "true",
   border: {
     type: "line"
   }
 });
 screen.append(eventBox);
+
+const notesBox = blessed.box({
+  width: "25%",
+  height: "85%",
+  top: "15%",
+  left: "75%",
+  border: {
+    type: "line"
+  }
+});
+screen.append(notesBox);
 
 screen.key(['escape', 'q', 'C-c'], () => {
   return process.exit(0);
@@ -67,23 +79,26 @@ screen.key(['escape', 'q', 'C-c'], () => {
   schedules.xfinity = await fetch(`https://www.nascar.com/cacher/${year}/2/race_list_basic.json`).then(res => res.json());
   schedules.cup = await fetch(`https://www.nascar.com/cacher/${year}/1/race_list_basic.json`).then(res => res.json());
   schedules.array = [].concat(schedules.trucks, schedules.xfinity, schedules.cup);
-  let scoringurl;
+  let seriesInfo = {};
   schedules.array.every(race => {
     const today = new Date();
     const raceDate = new Date(race.tunein_date);
     if (today.getDate() ==  raceDate.getDate() && today.getMonth() ==  raceDate.getMonth() && today.getFullYear() ==  raceDate.getFullYear()) {
-      scoringurl = `https://www.nascar.com/live/feeds/series_${race.series_id}/${race.race_id}/live_feed.json`;
-      return false;
+      seriesInfo.found = true;
+      seriesInfo.sid = race.series_id;
+      seriesInfo.rid = race.race_id;
+      if (!race.margin_of_victory) return false;
     }
     return true
   });
 
-  if (!scoringurl) {
+  if (!seriesInfo.found) {
     eventBox.setText("No race found for today");
     screen.render();
   } else {
     while (true) {
-      const scoring = await fetch(scoringurl).then(res => res.json());
+      const scoring = await fetch(`https://www.nascar.com/live/feeds/series_${seriesInfo.sid}/${seriesInfo.rid}/live_feed.json`).then(res => res.json());
+      const racenotes = await fetch(`https://www.nascar.com/cacher/${year}/${seriesInfo.sid}/${seriesInfo.rid}/lap-notes.json`).then(res => res.json()).catch(err => {return {laps: {}}});
   
       eventBox.setText(`${scoring.run_name}\n${scoring.track_name}`);
   
@@ -119,7 +134,13 @@ screen.key(['escape', 'q', 'C-c'], () => {
         else if (drivers[i].status == 3) delta = "Out"; 
         else if (drivers[i].delta.toString().indexOf("-") != -1) delta = `${drivers[i].delta} lap${Math.abs(drivers[i].delta) == 1 ? "" : "s"}`;
         else delta = `-${drivers[i].delta}`;
-        scoringBox.setLine(i, `${drivers[i].running_position.toString().padStart(2, " ")}.\t${drivers[i].vehicle_number.toString().padStart(2, " ")}\t${drivers[i].driver.full_name.replace(" #", "").replace("(i)", "").padEnd(40, " ")}\t${delta.padEnd(10, " ")}`);
+        scoringBox.setLine(i, `${drivers[i].running_position.toString().padStart(2, " ")}.\t#${drivers[i].vehicle_number.toString().padEnd(2, " ")}\t${drivers[i].driver.full_name.replace(" #", "").replace("(i)", "").padEnd(40, " ")}\t${delta.padEnd(10, " ")}`);
+      }
+
+      var j = 0;
+      for (const [lap, notes] of Object.entries(racenotes.laps)) {
+        notesBox.setLine(j, `Lap ${lap}: ${notes[0].Note}\n`);
+        j++;
       }
   
       screen.render();
